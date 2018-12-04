07ef8c6601d50dfb3585085691fba7ac19fe74b2
Deepak Tiwari <tiwarid99@gmail.com>
2018-12-04 19:50:55 +0530

    Added models for implementing Directory Structure

--- ide/models.py
+++ ide/models.py
@@ -6,3 +6,17 @@ class Chat(models.Model):
     message = models.TextField()
     RefProject = models.ForeignKey(Project, on_delete=models.CASCADE)
 
+class Source(models.Model):
+    RefProject = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+class Directory(models.Model):
+    Name = models.CharField(max_length=20)
+    Parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+    inSource = models.BooleanField()
+    SourceFolder = models.ForeignKey(Source, on_delete=models.CASCADE, null=True, blank=True)
+
+class File(models.Model):
+    Name = models.CharField(max_length=20)
+    Language = models.CharField(max_length=15)
+    FirbaseID = models.CharField(max_length=20)
+    Parent = models.ForeignKey(Directory, on_delete=models.CASCADE)
---
+++ static/js/firebase.js
@@ -0,0 +1,40 @@
+function init() {
+
+        setInterval(getMessages, 200000, id);
+
+        var config = {
+            apiKey: "AIzaSyBI8uzW2FaBTe_NCceJbgFsNxcz_EeBOUw",
+            authDomain: "collabide-2350f.firebaseapp.com",
+            databaseURL: "https://collabide-2350f.firebaseio.com"
+        };
+        firebase.initializeApp(config);
+        //// Get Firebase Database reference.
+        var firepadRef = getExampleRef();
+        //// Create ACE
+        var editor = ace.edit("firepad-container");
+        editor.setTheme("ace/theme/github");
+        var session = editor.getSession();
+        session.setUseWrapMode(true);
+        session.setUseWorker(false);
+        session.setMode("ace/mode/javascript");
+        //// Create Firepad.
+        var firepad = Firepad.fromACE(firepadRef, editor, {
+            defaultText: '// JavaScript Editing with Firepad!\nfunction go() {\n  var message = "Hello, world.";\n  console.log(message);\n}'
+        });
+    }
+    // Helper to get hash from end of URL or generate a random one.
+    function getExampleRef() {
+        var ref = firebase.database().ref();
+        var hash = window.location.hash.replace(/#/g, '');
+
+        if (hash) {
+            ref = ref.child(hash);
+        } else {
+            ref = ref.push(); // generate unique location.
+            window.location = window.location + '#' + ref.key; // add it as a hash to the URL.
+        }
+        if (typeof console !== 'undefined') {
+            console.log('Firebase data: ', ref.toString());
+        }
+        return ref;
+    }
